3987d8e3a622e5af0f7b598b171d5860a49175c9
GreatBrandon <brandon2005j@gmail.com>
2024-11-25 22:18:09 +0000

    Fixed code

--- src/com/flights/SQLtest.java
+++ src/com/flights/SQLtest.java
@@ -4,21 +4,23 @@ import java.sql.*;
 
 public class SQLtest {
     public static void main(String[] args) throws Exception{
-        String url = "jdbc:mysql://localhost:3306/flights";
-        String username = "project";
-        String password = "project";
-        String query = "SELECT * FROM booking";
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-
-        Connection con = DriverManager.getConnection(url, username, password);
+        final String URL = "jdbc:mysql://localhost:3306/flights_project";
+        final String USERNAME = "project";
+        final String PASSWORD = "project";
+        Class.forName("com.mysql.cj.jdbc.Driver"); // import jar file if error
+        Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         System.out.println("Connection established successfully");
+
+        String query = "SELECT * FROM booking";
         Statement st = con.createStatement();
+//        st.executeUpdate("DROP TABLE booking"); // should throw access denied error
+        st.executeQuery(query);
+
         ResultSet rs = st.executeQuery(query);
         rs.next();
         String name = rs.getString("email");
         System.out.println(name);
+        // close connection
         st.close();
         con.close();
         System.out.println("Connection closed");
