459eea6f40c7206337edc02c6c397f65e2c6cd10
Oliwier Jakubiec <C00296807@setu.ie>
2024-11-25 22:37:21 +0000

    updates

--- src/com/flights/SQLTest.java
+++ src/com/flights/SQLTest.java
@@ -2,22 +2,17 @@ package com.flights;
 
 import java.sql.*;
 
-public class SQLtest {
+public class SQLTest {
     public static void main(String[] args) throws Exception{
         final String URL = "jdbc:mysql://localhost:3306/flights_project";
         final String USERNAME = "project";
         final String PASSWORD = "project";
         Class.forName("com.mysql.cj.jdbc.Driver"); // import jar file if error
         Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        String url = "jdbc:mysql://localhost:3306/flights_project";
-        String username = "project";
-        String password = "project";
         String query = "SELECT * FROM booking";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-
-        Connection con = DriverManager.getConnection(url, username, password);
         System.out.println("Connection established successfully");
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(query);
