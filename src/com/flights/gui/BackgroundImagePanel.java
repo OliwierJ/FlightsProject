0b7ccd14aea468ca63c444c4c6b37e7dd9cf8db1
OliwierJ <162373475+OliwierJ@users.noreply.github.com>
2025-02-15 21:45:43 +0000

    Background image added

---
+++ src/com/flights/gui/BackgroundImagePanel.java
@@ -0,0 +1,20 @@
+package com.flights.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BackgroundImagePanel extends JPanel {
+    private Image backgroundImage;
+
+    // Constructor to load the image
+    public BackgroundImagePanel(String imagePath) {
+        backgroundImage = new ImageIcon(imagePath).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+
+}
